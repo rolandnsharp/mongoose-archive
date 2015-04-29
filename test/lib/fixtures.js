874a88051243901ff8d37a539e0293fc5fb5a7b1
Roland Sharp <rolandnsharp@gmail.com>
2015-04-30 09:28:30 +1000

    Add fixtures test data.

---
+++ test/lib/fixtures.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
+var Test = require('./model');
+
+var test = {
+    default: {
+        _id: new ObjectId(),
+        name: 'default'
+    },
+    archived: {
+        _id: new ObjectId(),
+        name: 'archived',
+        archived: true,
+        archivedAt: new Date()
+    },
+    test1: {
+        _id: new ObjectId(),
+        name: 'test1'
+    },
+    test2: {
+        _id: new ObjectId(),
+        name: 'test2'
+    }
+};
+
+module.exports = {
+    test: test
+};
